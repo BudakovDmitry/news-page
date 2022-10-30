f10dc7adc1933296dac45a2ce77b60ed9bd626fb
Budakov Dmitry <budakov.it@gmail.com>
2022-10-30 23:30:16 +0200

    Update rubrics adaptive

--- src/components/Rubric/index.tsx
+++ src/components/Rubric/index.tsx
@@ -28,49 +28,47 @@ const Rubric = ({ rubric }: RubricProps) => {
         />
       )}
       <Styled.RubricContentContainer>
-        {!isMobile() ||
-          (activeFilter === 1 && (
-            <Styled.NewsWithPhotoContainer>
-              <Styled.LargeNews>
-                <PhotoNews
-                  large={!isMobile() ? true : false}
-                  image={rubric.news.mainLargeNews[0].image}
-                  date={rubric.news.mainLargeNews[0].date}
-                  text={rubric.news.mainLargeNews[0].text}
-                  type={rubric.news.mainLargeNews[0].type}
-                  typeIcon={rubric.news.mainLargeNews[0].typeIcon}
+        {(!isMobile() || activeFilter === 1) && (
+          <Styled.NewsWithPhotoContainer>
+            <Styled.LargeNews>
+              <PhotoNews
+                large={!isMobile() ? true : false}
+                image={rubric.news.mainLargeNews[0].image}
+                date={rubric.news.mainLargeNews[0].date}
+                text={rubric.news.mainLargeNews[0].text}
+                type={rubric.news.mainLargeNews[0].type}
+                typeIcon={rubric.news.mainLargeNews[0].typeIcon}
+              />
+            </Styled.LargeNews>
+            {rubric.news.mainSmallNews.map(news => (
+              <PhotoNews
+                key={news.id}
+                image={news.image}
+                date={news.date}
+                text={news.text}
+              />
+            ))}
+          </Styled.NewsWithPhotoContainer>
+        )}
+        {(!isMobile() || activeFilter === 0) && (
+          <Styled.RubricTextNewsContainer>
+            <Styled.RubricTextNewsHeader>
+              Новини розділу
+            </Styled.RubricTextNewsHeader>
+            <NewsList news={rubric.news.textNews.slice(0, 5)} />
+            <Styled.RubricTextNewsMoreContainer>
+              <Styled.RubricTextNewsMoreText>
+                Більше
+              </Styled.RubricTextNewsMoreText>
+              <Styled.RubricTextNewsMoreLink>
+                <Styled.RubricTextNewsMoreLinkImage
+                  src={require('src/images/arrow-black-icon.png')}
+                  alt="arrow-right"
                 />
-              </Styled.LargeNews>
-              {rubric.news.mainSmallNews.map(news => (
-                <PhotoNews
-                  key={news.id}
-                  image={news.image}
-                  date={news.date}
-                  text={news.text}
-                />
-              ))}
-            </Styled.NewsWithPhotoContainer>
-          ))}
-        {!isMobile() ||
-          (activeFilter === 0 && (
-            <Styled.RubricTextNewsContainer>
-              <Styled.RubricTextNewsHeader>
-                Новини розділу
-              </Styled.RubricTextNewsHeader>
-              <NewsList news={rubric.news.textNews.slice(0, 5)} />
-              <Styled.RubricTextNewsMoreContainer>
-                <Styled.RubricTextNewsMoreText>
-                  Більше
-                </Styled.RubricTextNewsMoreText>
-                <Styled.RubricTextNewsMoreLink>
-                  <Styled.RubricTextNewsMoreLinkImage
-                    src={require('src/images/arrow-black-icon.png')}
-                    alt="arrow-right"
-                  />
-                </Styled.RubricTextNewsMoreLink>
-              </Styled.RubricTextNewsMoreContainer>
-            </Styled.RubricTextNewsContainer>
-          ))}
+              </Styled.RubricTextNewsMoreLink>
+            </Styled.RubricTextNewsMoreContainer>
+          </Styled.RubricTextNewsContainer>
+        )}
       </Styled.RubricContentContainer>
       {isMobile() && activeFilter !== 0 && (
         <Styled.RubricTextNewsMoreContainer>
